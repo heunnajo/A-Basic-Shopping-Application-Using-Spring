dcc1d613383b612ae4c8ff4d494676404d4cd3d3
조흔나 <gms950831@gmail.com>
2024-05-30 17:39:29 +0900

    Update OrderApp.java

--- core/src/main/java/shopping/core/order/OrderApp.java
+++ core/src/main/java/shopping/core/order/OrderApp.java
@@ -1,6 +1,9 @@
 package shopping.core.order;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import shopping.core.AppConfig;
+import shopping.core.MemberApp;
 import shopping.core.member.Grade;
 import shopping.core.member.Member;
 import shopping.core.member.MemberService;
@@ -8,18 +11,21 @@ import shopping.core.member.MemberServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
 
-//        MemberService memberService = new MemberServiceImpl(null);
-//        OrderService orderService = new OrderServiceImpl(null,null);
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        //spring 으로 전환
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId,"memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
-        System.out.println("order = " + order.toString());
+        Order order = orderService.createOrder(memberId, "itemA", 30000);
+        System.out.println("order = " + order);
     }
 }
