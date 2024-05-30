c7dae926f4290a532b8ac237e7d9ef9f6de8764c
조흔나 <gms950831@gmail.com>
2024-05-30 17:39:32 +0900

    Update MemberApp.java

--- core/src/main/java/shopping/core/MemberApp.java
+++ core/src/main/java/shopping/core/MemberApp.java
@@ -1,5 +1,7 @@
 package shopping.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import shopping.core.member.Grade;
 import shopping.core.member.Member;
 import shopping.core.member.MemberService;
@@ -7,9 +9,12 @@ import shopping.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        //MemberService memberService = new MemberServiceImpl();
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        //AppConfig 설정 정보를 가지고 스프링이 @Bean 들을 스프링 컨테이너에 넣어서 관리해준다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //위에서 직접 찾아온 걸 스프링 컨테이너 통해서 찾아와야 한다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member memberA = new Member(1L, "memberA", Grade.VIP);
         memberService.join(memberA);
