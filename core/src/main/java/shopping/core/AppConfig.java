ece28e90b73c4111bcc93815d3d35063135efd96
조흔나 <gms950831@gmail.com>
2024-05-30 17:39:40 +0900

    Update AppConfig.java

--- core/src/main/java/shopping/core/AppConfig.java
+++ core/src/main/java/shopping/core/AppConfig.java
@@ -1,22 +1,36 @@
 package shopping.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import shopping.core.discount.DiscountPolicy;
 import shopping.core.discount.FixDiscountPolicy;
+import shopping.core.discount.RateDiscountPolicy;
+import shopping.core.member.MemberRepository;
 import shopping.core.member.MemberService;
 import shopping.core.member.MemberServiceImpl;
 import shopping.core.member.MemoryMemberRepository;
 import shopping.core.order.OrderService;
 import shopping.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository()); //생성자 주입 : 생성자를 통해 의존성 주입
+        return new MemberServiceImpl(memberRepository()); //생성자 주입 : 생성자를 통해 의존성 주입
     }
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository(); //생성자 주입 : 생성자를 통해 의존성 주입
+    }
+    @Bean
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy()); //생성자 주입 : 생성자를 통해 의존성 주입
+        return new OrderServiceImpl(memberRepository(), discountPolicy()); //생성자 주입 : 생성자를 통해 의존성 주입
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+
     }
 }
