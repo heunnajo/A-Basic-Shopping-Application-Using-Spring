c71e01ef05d1bcc1202498faf0a828b5d416f140
조흔나 <gms950831@gmail.com>
2024-07-02 10:27:32 +0900

    Create ApplicationContextInfoTest.java

---
+++ core/src/main/java/shopping/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,36 @@
+package shopping.core.beanfind;
+
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import shopping.core.AppConfig;
+import org.junit.jupiter.api.Test;
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            //System.out.println("beanDefinitionName = " + beanDefinitionName +" object = "+bean);
+        }
+    }
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //(스프링의 빈이 아니라)내가 등록한 빈
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("My bean = " + beanDefinitionName +" object = "+bean);
+            }
+        }
+    }
+
+}
